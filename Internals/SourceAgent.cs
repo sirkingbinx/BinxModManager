8a40ba2b3d31744ad2ca14bd3888fe4960459f3b
Bingus <bendawsdev@gmail.com>
2025-04-10 09:58:30 -0500

    fix everything using T++

--- Internals/SourceAgent.cs
+++ Internals/SourceAgent.cs
@@ -67,57 +67,71 @@ namespace PygmyModManager.Internals
 
         public static List<ReleaseInfo> GatherSources()
         {
-            if (!File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\sources.pygmysources"))
+            if (!File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\sources.txp"))
             {
                 // sets a default file
                 List<string> newFile = new();
 
-                newFile.Add("# This is the sources file for PygmyModManager");
+                newFile.Add("# This is the sources file for Binx's Mod Manager");
                 newFile.Add("# You can make comments by using \"#\"");
+                newFile.Add("# Everything's in Txt++ format");
+                newFile.Add("");
+                newFile.Add("# To put the default MMM list, add a line that says $default");
                 newFile.Add("");
                 newFile.Add("https://raw.githubusercontent.com/The-Graze/MonkeModInfo/master/modinfo.json");
 
-                File.WriteAllLines(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\sources.pygmysources", newFile);
+                File.WriteAllLines(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\sources.txp", newFile);
             }
 
-            sources = ParseSourceFile(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\sources.pygmysources");
-            
-            if (sources.Count == 0)
-            {
-                sources.Add("https://raw.githubusercontent.com/The-Graze/MonkeModInfo/master/modinfo.json");
-            }
+            TextPlusPlus.DeclareVariable("default", "https://raw.githubusercontent.com/The-Graze/MonkeModInfo/master/modinfo.json");
+            // $default ^^^^
 
-            List<ReleaseInfo> mods = new();
-            
-            foreach (string sourceURL in sources)
-            {
-                var decodedMods = JSON.Parse(GatherWebContent(sourceURL));
-                var allMods = decodedMods.AsArray;
+            int success;
+            string errorMsg;
 
-                for (int i = 0; i < allMods.Count; i++)
+            sources, success, errorMsg = TextPlusPlus.ParseSourceFile(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\sources.txp");
+            
+            if (success == 0) {
+                if (sources.Count == 0)
                 {
-                    JSONNode current = allMods[i];
-                    ReleaseInfo release = new ReleaseInfo(current["name"], current["author"], current["group"], current["download_url"], current["install_location"], current["git_path"], current["dependencies"].AsArray);
-                    mods.Add(release);
+                    sources.Add("https://raw.githubusercontent.com/The-Graze/MonkeModInfo/master/modinfo.json");
                 }
+
+                List<ReleaseInfo> mods = new();
+                
+                foreach (string sourceURL in sources)
+                {
+                    var decodedMods = JSON.Parse(GatherWebContent(sourceURL));
+                    var allMods = decodedMods.AsArray;
+
+                    for (int i = 0; i < allMods.Count; i++)
+                    {
+                        JSONNode current = allMods[i];
+                        ReleaseInfo release = new ReleaseInfo(current["name"], current["author"], current["group"], current["download_url"], current["install_location"], current["git_path"], current["dependencies"].AsArray);
+                        mods.Add(release);
+                    }
+                }
+
+                // trusted source info
+                var srclist = JSON.Parse(GatherWebContent("https://raw.githubusercontent.com/sirkingbinx/PygmyModManager/refs/heads/master/trusted_sources.json"));
+                var allSrc = srclist.AsArray;
+
+                var thisCurrent = allSrc[0];
+                Pygmy_API_Endpoint = "https://api.github.com/repos/sirkingbinx/PygmyModManager/";
+                Repo_API_Endpoint = "https://api.github.com/repos/";
+
+                for (int i = 0; i < allSrc.Count; i++)
+                {
+                    JSONNode current = allSrc[i];
+                    SourceInfo release = new SourceInfo(current["title"], current["link"]);
+                    TrustSourceList.Add(release);
+                }
+
+                return mods;
+            } else {
+                MessageBox.Show(errorMsg, "Error parsing sources.txp", MessageBoxButtons.OK, MessageBoxIcons.Error);
+                Application.Exit();
             }
-
-            // trusted source info
-            var srclist = JSON.Parse(GatherWebContent("https://raw.githubusercontent.com/sirkingbinx/PygmyModManager/refs/heads/master/trusted_sources.json"));
-            var allSrc = srclist.AsArray;
-
-            var thisCurrent = allSrc[0];
-            Pygmy_API_Endpoint = "https://api.github.com/repos/sirkingbinx/PygmyModManager/";
-            Repo_API_Endpoint = "https://api.github.com/repos/";
-
-            for (int i = 0; i < allSrc.Count; i++)
-            {
-                JSONNode current = allSrc[i];
-                SourceInfo release = new SourceInfo(current["title"], current["link"]);
-                TrustSourceList.Add(release);
-            }
-
-            return mods;
         }
     }
 }
